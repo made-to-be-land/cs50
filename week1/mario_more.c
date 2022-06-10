fc528beed50e2ded233246f5e333f26b0b6dc1e6
Maksym <made-to-be-land>
2022-06-10 18:49:45 +0300

    style

--- week1/mario_more.c
+++ week1/mario_more.c
@@ -2,32 +2,31 @@
 
 int main(void)
 {
-int height, row, space, column;
+    int height, row, space, column;
 
-do
-{
-    height = get_int("height = ");
-}
-while (height < 1 || height > 8);
-
-for(row = 0; row < height; row++)
-{
-    for(space = 0; space < height - row - 1; space++)
+    do
     {
-        printf(" ");
+        height = get_int("height = ");
     }
-    for(column = 0; column <= row; column++)
+    while (height < 1 || height > 8);
+
+    for (row = 0; row < height; row++)
     {
-        printf("#");
+        for (space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-
-printf("  ");
-
-for(column = 0; column <= row; column++)
-    {
-        printf("#");
-    }
-printf("\n");
 }
-}
-
