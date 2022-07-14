e8a64370167db7b7e62e16d8ac65aa0faad6b73b
Maksym <made-to-be-land>
2022-07-14 17:02:33 +0300

    gray filter

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -1,8 +1,20 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        for (int colomn = 0; colomn < width; colomn++)
+        {
+            int rgbtGray = round((image[row][colomn].rgbtBlue + image[row][colomn].rgbtGreen + image[row][colomn].rgbtRed) / 3.0);
+
+            image[row][colomn].rgbtBlue = rgbtGray;
+            image[row][colomn].rgbtGreen = rgbtGray;
+            image[row][colomn].rgbtRed = rgbtGray;
+        }
+    }
     return;
 }
 
